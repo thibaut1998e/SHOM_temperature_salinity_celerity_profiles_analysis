5215e6b6e196a65c52a59bfd8d9866dcb8f3fee2
Anthony Frion <58110567+anthony-frion@users.noreply.github.com>
2020-10-22 13:52:10 +0200

    plot the surface temperatures
    The figure now colors the profile locations according to their surface temperatures.

--- plot_profile_locations.py
+++ plot_profile_locations.py
@@ -29,7 +29,10 @@ with open('ts_profiles.pkl', 'rb') as f:
 lats = [prof.lat for prof in PROFS]
 lons = [prof.lon for prof in PROFS]
 
-plt.scatter(lons, lats, s=0.5)
+surface_temps = [prof.temp[-1] for prof in PROFS]
+
+plt.scatter(lons, lats, s=0.5, c=surface_temps)
+plt.colorbar()
 
 import os
 
@@ -41,5 +44,7 @@ from mpl_toolkits.basemap import Basemap
 
 map = Basemap(llcrnrlon=-10, llcrnrlat=25, urcrnrlon=40, urcrnrlat=50)
 map.drawcoastlines(linewidth=0.5)
+
+print("Plotting all the profile locations with their associated surface temperatures.")
 
 plt.show()
