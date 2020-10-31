f94edd5774e1d1978229fdff114616cb271ab51f
thibaut1998e <58691353+thibaut1998e@users.noreply.github.com>
2020-10-31 12:59:58 +0100

    non uniform prob of selection

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import pickle
 import numpy as np
 import os
 import  matplotlib.pyplot as plt
-
+import random as rd
 
 #HelloWorld
 # profiles class
@@ -44,18 +44,8 @@ def get_date(PROFS):
     return np.array([transform_string_date_into_integer(p.date) for p in PROFS])
 
 
-def get_profiles(p=0.05):
-    #p = proportion of validation data
-    with open('ts_profiles.pkl', 'rb') as f:
-        PROFS = pickle.load(f)
-    np.random.shuffle(PROFS)
-    n = len(PROFS)
-    valid_PROFS = PROFS[:int(n*p)]
-    train_PROFS = PROFS[int(n*p):]
-    return PROFS, train_PROFS, valid_PROFS
 
-
-def make_grid(lat_min=32, lat_max=44, lat_step=1, long_min=-4, long_max=35,
+def make_grid(lat_min=31, lat_max=45, lat_step=1, long_min=-4, long_max=35,
                       long_step=1):
     longitudes = np.arange(long_min, long_max, long_step)
     latitudes = np.arange(lat_min, lat_max, lat_step)
@@ -63,58 +53,7 @@ def make_grid(lat_min=32, lat_max=44, lat_step=1, long_min=-4, long_max=35,
     return grid
 
 
-def density_profiles(PROFS, radius=1, save_loc='profile_density.txt', **args_grid):
-    """save the densities of profiles in the file save location
-    first a grid is created (with thee arguments **args_grid), then for each point of the grid
-    the density is computed by counting the number profiles located in a circle (radius odf the circle given in parameter)"""
-    if os.path.exists(save_loc):
-        q = input(f'delete file {save_loc} (y)')
-        if q == 'y':
-            os.remove(save_loc)
-        else:
-            exit()
-    f = open(save_loc, "w")
-    print('long\tlat\tdensity', file=f)
-    long_lats = get_long_lats(PROFS)
-    grid = make_grid(**args_grid)
-    print('number of point', len(grid))
-    cpt = 0
-    for point in grid:
-        cpt += 1
-        print(cpt)
-        nb_points = 0
-        for p in long_lats:
-            if distance(point, p) < radius:
-                nb_points += 1
-        d = nb_points / (np.pi*radius**2)
-        long, lat = point
-        print(f'{long}\t{lat}\t{d}', file=f)
 
-
-def read_density_txt(txt_path='profile_density.txt'):
-    """read the txt file saved by the function density profiles, return a 2D array shape (2, Nb_profiles) containing lat/long
-    coordinates of the grid, and a 1D array containing the associated density"""
-    with open(txt_path) as f:
-        lines = f.readlines()
-    longs_lats = []
-    densities = []
-    for line in lines[1:]:
-        long, lat, d = line.split('\t')
-        long, lat, d = float(long), float(lat), float(d[:-2])
-        longs_lats.append([long, lat])
-        densities.append(d)
-    return np.array(longs_lats), np.array(densities)
-
-
-def density_profiles_nn_interpolation(profile_densities, point,
-                                      long_min=-4, long_max=35, long_step=0.1, lat_min=32, lat_step=0.1):
-    """returns the density of profiles at any point on the map, by finding the nearest neighbour of the point
-    it assumes that the grid of profile_densities is defined by long_min, long_max, long_step, lat_min, lat_max, lat_step.
-    ex : point [5.87, 41.23] : it will return the density of profile at location [5.9, 41.2]"""
-    long_nn, lat_nn = int(point[0]/long_step+0.5)*long_step, int(point[1]/lat_step+0.5)*lat_step
-    idx = (lat_nn - lat_min)/lat_step * (long_max-long_min)/long_step + (long_nn - long_min)/long_step
-    idx = int(idx)
-    return profile_densities[idx]
 
 
 
@@ -182,24 +121,141 @@ def var(L) :
     m = mean(L)
     return mean([(x - m) **2 for x in L])
 
+def density_profiles(PROFS, radius=1, save_loc='profile_density.txt', **args_grid):
+    """save the densities of profiles in the file save location
+    first a grid is created (with thee arguments **args_grid), then for each point of the grid
+    the density is computed by counting the number profiles located in a circle (radius odf the circle given in parameter)"""
+    if os.path.exists(save_loc):
+        q = input(f'delete file {save_loc} (y)')
+        if q == 'y':
+            os.remove(save_loc)
+        else:
+            exit()
+    f = open(save_loc, "w")
+    print('long\tlat\tdensity', file=f)
+    long_lats = get_long_lats(PROFS)
+    grid = make_grid(**args_grid)
+    print('number of point', len(grid))
+    cpt = 0
+    for point in grid:
+        cpt += 1
+        print(cpt)
+        nb_points = 0
+        for p in long_lats:
+            if distance(point, p) < radius:
+                nb_points += 1
+        d = nb_points / (np.pi*radius**2)
+        long, lat = point
+        print(f'{long}\t{lat}\t{d}', file=f)
+
+
+def read_density_txt(txt_path='profile_density.txt'):
+    """read the txt file saved by the function density profiles, return a 2D array shape (2, Nb_profiles) containing lat/long
+    coordinates of the grid, and a 1D array containing the associated density"""
+    with open(txt_path) as f:
+        lines = f.readlines()
+    longs_lats = []
+    densities = []
+    for line in lines[1:]:
+        long, lat, d = line.split('\t')
+        long, lat, d = float(long), float(lat), float(d[:-2])
+        longs_lats.append([long, lat])
+        densities.append(d)
+    return np.array(longs_lats), np.array(densities)
+
+
+def density_profiles_nn_interpolation(profile_densities, point,
+                                      long_min=-4, long_max=35, long_step=0.1, lat_min=31, lat_step=0.1):
+    """returns the density of profiles at any point on the map, by finding the nearest neighbour of the point
+    it assumes that the grid of profile_densities is defined by long_min, long_max, long_step, lat_min, lat_max, lat_step.
+    ex : point [5.87, 41.23] : it will return the density of profile at location [5.9, 41.2]"""
+
+    long_nn, lat_nn = int(point[0]/long_step+0.5)*long_step, int(point[1]/lat_step+0.5)*lat_step
+    idx = (lat_nn - lat_min)/lat_step * (long_max-long_min)/long_step + (long_nn - long_min)/long_step
+    idx = int(idx)
+    return profile_densities[idx]
+
+
+def get_profiles(file='ts_profiles.pkl', p=0.05, density_grid=None):
+    """return the list of profiles in file and two set train_PROFS an valid_PROFS
+     if density_grid is None it selects the profiles in valid_PROFS following an uniform probability distribution
+     otherwise each profile is selected with a probability inversly proportional to the density of profiles at its location
+     p is the proportion of validation data to use"""
+    #p = proportion of validation data
+    with open(file, 'rb') as f:
+        PROFS = pickle.load(f)
+    n = len(PROFS)
+    if density_grid is None:
+        np.random.shuffle(PROFS)
+        valid_PROFS = PROFS[:int(n*p)]
+        train_PROFS = PROFS[int(n*p):]
+
+    else:
+        nb_validation = n*p
+        long_lats = get_long_lats(PROFS)
+        densities = [density_profiles_nn_interpolation(density_grid, p) for p in long_lats]
+        C = nb_validation/sum([1/d for d in densities]) #we choose C such that the expectation of the number of validation
+        #profiles is nb_validation = number of profiles * proportion of validation data
+        valid_PROFS = []
+        valid_PROFS_idx = []
+        for i in range(len(PROFS)):
+            select_prob = C/densities[i]
+            if rd.random() < select_prob:
+                valid_PROFS_idx.append(i)
+                valid_PROFS.append(PROFS[i])
+        train_PROFS = [PROFS[idx] for idx in range(n) if idx not in valid_PROFS_idx]
+    print('number of validation profiles', len(valid_PROFS))
+    return PROFS, train_PROFS, valid_PROFS
+
+
+def plot_density_histogram(PROFS, density_grid, title, range=None):
+    long_lats = get_long_lats(PROFS)
+    densities = [density_profiles_nn_interpolation(density_grid, p) for p in long_lats]
+    plt.hist(densities, range=range)
+    plt.xlabel('profile density')
+    plt.ylabel('nb of profiles')
+    plt.title(title)
+    plt.show()
+
 
 
 
 if __name__ == '__main__':
 
-    PROFS, _, _ = get_profiles()
-    #density_profiles(PROFS, radius=1, lat_step=0.1, long_step=0.1) #save txt file with densities on a grid
+    #PROFS, _, _ = get_profiles()
+    #density_profiles(PROFS, radius=1, lat_step=0.1, long_step=0.1) #save txt file with densities on a gridy
     long_lats, densities = read_density_txt() #get the saved values of densities
+    print('début')
+    _, _, valid_PROFS = get_profiles(density_grid=densities)
+    plot_density_histogram(valid_PROFS, densities,
+                           title='validation profile densities with probability selection inversly proportional to density',
+                           range=(0, 50))
+    _, _, valid_PROFS = get_profiles()
+    plot_density_histogram(valid_PROFS, densities,
+                           title='validation profile densities with uniform probability of selection')
+
+
+
+
+
     long = 22.31
     lat = 34.65
     point = np.array([long, lat])
     density_at_point = density_profiles_nn_interpolation(densities, point)
-
+    
     print(f'the density of profiles at point {point} is : {density_at_point}')
     from plot_profile_locations import plot_data
     print('plot profiles densities')
+
     plot_data(long_lats.T[0], long_lats.T[1], densities, color_log_scale=True, title='density of profiles (log scale)',
               save_location='density_of_profiles')
+
+    longs = np.arange(-4, 35, 0.01)
+    points = [[long, 38] for long in longs]
+    plt.plot(longs, [density_profiles_nn_interpolation(densities, p) for p in points])
+    plt.show()
+    
+
 
 
 
