b464376e8c662bd1e17f99770618e57b742ad951
Anthony Frion <58110567+anthony-frion@users.noreply.github.com>
2020-11-02 13:02:14 +0100

    Back to previous + new ideas
    New ideas of adaptive sigmas :
    - using the 'densities' from utils and figure out a function that attributes sigmas according to the densities (it has to be decreasing, but it's not easy to find)
    - computing the distance to the closest known point and defining sigma according to it (currently, sigma is the double of this distance, other values could be tried)
    - (future) using the mean distance to the k closest known points (k = 3, 5, 10, 20 ?)

--- Interpolation.py
+++ Interpolation.py
@@ -25,19 +25,42 @@ def interpolation_map(train_long_lat, labels, test_long_lat, const=0, threshold=
 
 
 def interpolation_validation(train_long_lat, labels, valid_long_lat, valid_labels, valid_dates, average_temps,
-                             threshold=15, sigma=15):
+                             threshold=15, sigma=None, lat_reg=None, densities=[]):
     """compute the average error of the interpolation on valid_long_lat"""
     print(f'processing {len(valid_long_lat)} validation data')
-
-    valid_predictions = [gaussian_weight_interpolation(p, train_long_lat, labels, threshold, sigma)
+    global cpt
+    cpt = 0
+    # The following if/elif/else instructions prepare the list of standard deviations sigmas
+    if sigma != None :
+        sigmas = [sigma for i in range(len(valid_long_lat))]
+    elif densities != [] :
+        sigmas = []
+        for i,p in enumerate(valid_long_lat) :
+            density = density_profiles_nn_interpolation(densities, p)
+            #print(density)
+            #sigmas.append(np.exp(-np.log(1+np.log(1+density))) * .3)
+            sigmas.append(np.sqrt(np.exp(-np.log((1+density)))))
+        print(sigmas)
+    else :
+        sigmas = [None for i in range(len(valid_long_lat))]
+    valid_predictions = [gaussian_weight_interpolation(p, train_long_lat, labels, threshold, sigmas[i])
                          + average_temps[valid_dates[i]-1] for i,p in enumerate(valid_long_lat)]
-
-    errors = np.abs(np.array(valid_predictions)-np.array(valid_labels))
-    mean_error = np.mean(errors)
-    std_error = np.std(errors)
-    return errors, mean_error, std_error
-
-
+    # influence of the latitude, if needed
+    if lat_reg != None :
+        for i,p in enumerate(valid_long_lat) :
+            valid_predictions[i] += lat_reg.predict(np.array([[p[1]]]))[0]
+    
+    error = distance(valid_predictions, valid_labels, order=1) / len(valid_predictions)
+    
+    
+    #print([valid_predictions[i] - valid_labels[i] for i in range(len(valid_labels))])
+    if sigma == None and densities != [] :
+        print(f"Mean of sigmas : {mean(sigmas)}")
+        print("Weighted sum of errors :")
+        print(sum([abs(valid_predictions[i] - valid_labels[i]) * sigmas[i] for i in range(len(sigmas))]) / sum(sigmas))
+        print("Unweighted sum of errors :")
+        print(mean([abs(valid_predictions[i] - valid_labels[i]) for i in range(len(sigmas))]))
+    return error
 
 cpt = 0
 
@@ -54,32 +77,51 @@ def gaussian_weight_interpolation(p, X, Y, threshold, sigma):
     if cpt % 10 == 0:
         print(cpt)
     closest_point_values = []
+    closest_point_distances = []
     weights = []
-    for i,x in enumerate(X):
-        dist = distance(p, x)
-        if dist < threshold:
-            w = gauss(dist, sigma)
-            weights.append(w)
-            closest_point_values.append(Y[i])
-    closest_point_values = np.array(closest_point_values)
-    S = sum(weights)
-    weights = np.array([w / S for w in weights])
+    if sigma != None :
+        for i,x in enumerate(X):
+            dist = distance(p, x)
+            if dist < threshold:
+                w = gauss(dist, sigma)
+                weights.append(w)
+                closest_point_values.append(Y[i])
+        closest_point_values = np.array(closest_point_values)
+        S = sum(weights)
+        weights = np.array([w / S for w in weights])
+    else :
+        # if argument sigma is None then we define sigma as the double of the distance to the closest point
+        for i,x in enumerate(X):
+            dist = distance(p, x)
+            if dist < threshold:
+                closest_point_values.append(Y[i])
+                closest_point_distances.append(dist)
+        min_dist = min(closest_point_distances)
+        sigma = 2 * min_dist if min_dist > 0 else 0.001
+        
+        print(sigma)
+        for dist in closest_point_distances :
+            weights.append(gauss(dist, sigma))
+        closest_point_values = np.array(closest_point_values)
+        S = sum(weights)
+        weights = np.array([w / S for w in weights])
     y_pred = closest_point_values.dot(weights.T) #predicted value for p
     return y_pred
 
 
 
 
-
 if __name__ == '__main__':
-    from temperature_residuals import surface_temps_2, lons, lats, smoothed_Y, valid_profs
+    from temperature_residuals import surface_temps_2, surface_temps_3, lons, lats, smoothed_Y, valid_profs, reg
     #from utils import get_profiles, LIGHT_PROF
+
     day = 25
     lons = np.array(lons)
     lats = np.array(lats)
     surface_temps_2 = np.array(surface_temps_2)
-    print('mean of residuals (without removing lat influence)', np.mean(surface_temps_2))
-    print('std of residuals (without removing lat influence)', np.std(surface_temps_2))
+    surface_temps_3 = np.array(surface_temps_3)
+    #print('mean of residuals (without removing lat influence)', np.mean(surface_temps_2))
+    #print('std of residuals (without removing lat influence)', np.std(surface_temps_2))
     smoothed_Y = np.array(smoothed_Y)
     long_lat = np.array([lons, lats]).T
     title = f'predicted surface temperatures in the Mediterranean Sea at day {day}'
@@ -90,7 +132,7 @@ if __name__ == '__main__':
     valid_temps = get_surf_temp(valid_profs)
     valid_dates = get_date(valid_profs)
     long_lat_valid = np.array([valid_lon, valid_lat]).T
-    grid = make_grid()
+    long_lats, densities = read_density_txt() #get the saved values of densities
     import time
     t = time.time()
     #interpolation_map(long_lat, surface_temps_2, grid, const=const, title=title, save_location=save_location, show=True)
@@ -100,21 +142,15 @@ if __name__ == '__main__':
     print(error)
     print('temps écoulé', time.time() - t)
     """
-    list_sigma = [0.01, 0.02, 0.05, 0.07, 0.1, 0.2, 0.5, 0.7, 1, 2, 5, 7, 10, 20]
-
-    errors_against_sigma = [interpolation_validation(long_lat, surface_temps_2, long_lat_valid, valid_temps, valid_dates
-                                                     ,smoothed_Y, sigma=sig, threshold=10**10)[1] for sig in list_sigma]
-
-    plt.plot(list_sigma, errors_against_sigma)
-
-    plt.title('error against sigma with validation data selected with uniform distribution')
-    plt.ylabel('mean error (L1) (°C)')
-    plt.xlabel('sigma : standard deviation of gaussian kernel use to compute weights')
-    plt.xscale('log')
-    plt.show()
-
-    print('error for differnt values of sigma', errors_against_sigma)
-
+    #errors_against_sigma_2 = [interpolation_validation(long_lat, surface_temps_2, long_lat_valid, valid_temps, valid_dates
+    #                                                 ,smoothed_Y, sigma=None, threshold=3) for sig in [1]]
+    errors_against_sigma_3 = [interpolation_validation(long_lat, surface_temps_3, long_lat_valid, valid_temps, valid_dates
+                                                     ,smoothed_Y, sigma=.1, threshold=5, lat_reg=reg) for sig in [1]]
+    errors_against_sigma_4 = [interpolation_validation(long_lat, surface_temps_3, long_lat_valid, valid_temps, valid_dates
+                                                     ,smoothed_Y, sigma=None, threshold=5, lat_reg=reg, densities=[]) for sig in [1]]
+    # errors_against_sigma_3 uses a constant sigma of value 0.1
+    # errors_against_sigma_4 uses a sigma that depends on the distance to the closest point
+    print(errors_against_sigma_3, errors_against_sigma_4)
 
 
 
